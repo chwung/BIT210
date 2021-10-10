d6deb5258ffda01b9281a70962882bcb18804530
vincentkaw <91302085+vincentkaw@users.noreply.github.com>
2021-10-10 23:18:24 +0800

    lol

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,5 @@
 var patientArray = [];
+var staffArray = [];
 
 class patient{
     constructor(name,password, email, id){
@@ -9,9 +10,38 @@ class patient{
     }
 }
 
-var submit = document.getElementById("btnSubmit");
-submit.addEventListener("click", patientSubmit);
 
+function reset(){
+    document.getElementById("form").reset;
+}
+
+function validatePatient(){
+    let name = document.forms["patientForm"]["name"].value;
+    let password = document.forms["patientForm"]["password"].value;
+    let email = document.forms["patientForm"]["email"].value;
+    let id = document.forms["patientForm"]["id"].value;
+
+    if (name == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    else if (password == "") {
+        alert("Password must be filled out");
+        return false;
+    }
+    else if (email == "") {
+        alert("email must be filled out");
+        return false;
+    }
+    else if (id == "") {
+        alert("IC/Passport must be filled out");
+        return false;
+    }
+    else{
+        patientSubmit();
+        window.location.href = "Login.html";
+    }
+}
 
 function patientSubmit(){
     var name = document.getElementById("name");
@@ -21,6 +51,7 @@ function patientSubmit(){
 
     var newPatient = new patient(name, password, email, id);
 
+    
     patientArray.push(newPatient);
     console.log(patientArray);
 }
@@ -35,4 +66,21 @@ class staff{
         this.email = email;
         this.staffID = staffID;
     }
+}
+
+function staffSubmit(){
+    var centreName = document.getElementById("centreName");
+    var address = document.getElementById("address");
+    var name = document.getElementById("name");
+    var fullName = document.getElementById("fullName");
+    var password = document.getElementById("password");
+    var email = document.getElementById("email");
+    var staffID = document.getElementById("staffID");
+
+    var newStaff = new staff(centreName, address,name,fullName, password, email, staffID);
+
+    
+    staffArray.push(newStaff);
+    console.log(staffArray);
+    window.location.href = "Login.html";
 }
