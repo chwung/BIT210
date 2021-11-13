8d73f790d55398f8a0f5ab882be9423ac9795fb6
chwung <weichung35@gmail.com>
2021-11-13 17:18:15 +0800

    Update login.php

--- login.php
+++ login.php
@@ -11,27 +11,28 @@
 $sqlPatient = "SELECT * FROM PATIENTS WHERE username = '$username' AND password = '$password'";
 $patient = $connection->query($sqlPatient);
 
-$sqlStaff = "SELECT * FROM STAFF WHERE username = '$username' AND password = '$password'";
+$sqlStaff = "SELECT * FROM STAFFS WHERE username = '$username' AND password = '$password'";
 $staff = $connection->query($sqlStaff);
 
 if($patient -> num_rows > 0){
     $patientFlag = 1;
-} //else if($staff -> num_rows > 0) {
-    //$staffFlag = 1;
-//}
+}else if($staff -> num_rows > 0) {
+    $staffFlag = 1;
+}
 
 if($staffFlag == 0 && $patientFlag == 0){
     echo '<script type="text/javascript">
     alert("Username or Password wrong.");
     </script>';
-    header("location: login.html");
+    //header("location: login.html");
 } else if($patientFlag == 1) {
     $_SESSION['patientUsername'] = $username;
     $_SESSION['patientPassword  '] = $password;
-    header("location: PatientProfile.html");
+    //header("location: PatientProfile.html");
 } else if($staffFlag == 1){
     $_SESSION['staffUsername'] = $username;
     $_SESSION['staffPassword'] = $password;
-    header("location: StaffProfile.html");
+    echo "success";
+    //header("location: StaffProfile.html");
 }
 ?>
