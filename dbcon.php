471fa152997111ff660223b8adafee7b3c182e00
chwung <weichung35@gmail.com>
2021-11-13 18:35:40 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -4,8 +4,8 @@ $connection = new mysqli ("localhost", "root", "", "vaccess");
 if ($connection -> connect_error){
     die($connection -> connect_error);
 }else{
-    echo '<script type="text/javascript">';
-    echo 'alert("Connection success.")';
-    echo '</script>';
+    #echo '<script type="text/javascript">';
+    #echo 'alert("Connection success.")';
+    #echo '</script>';
 }
 ?>
