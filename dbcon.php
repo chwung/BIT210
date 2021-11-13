ccc058485026673a8172d89c0086b54eeecd048a
vincentkaw <91302085+vincentkaw@users.noreply.github.com>
2021-11-13 17:21:47 +0800

    new

--- dbcon.php
+++ dbcon.php
@@ -6,14 +6,8 @@ $connection = new mysqli ("localhost", "root", "", "vaccess");
 if ($connection -> connect_error){
     die($connection -> connect_error);
 }else{
-<<<<<<< HEAD
-    echo '<script type="text/javascript">';
-    echo 'alert("Connection success.")';
-    echo '</script>';
-=======
     echo"<script type="text/javascript">
     alert('Connection success.');
     </script>";
->>>>>>> 55ed211830450f76c8e76a6db1382f429425c328
 }
 ?>
