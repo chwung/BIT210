1a4111deef813a2c9ca9d328e100f1cdc639b18f
chwung <weichung35@gmail.com>
2021-11-16 19:44:45 +0800

    Update status.php

--- status.php
+++ status.php
@@ -2,6 +2,19 @@
     session_start();
 
     include("dbcon.php");
+
+    if(isset($_SESSION['patientUsername']))
+    {
+        $username = $_SESSION['patientUsername'];
+        $password = $_SESSION['patientPassword'];
+        $query = "SELECT * FROM patients WHERE username = '$username' AND password = '$password'";
+
+        $result = $connection->query($query);
+        if($result -> num_rows > 0)
+        {
+            $user_data = $result -> fetch_assoc();
+        }
+    }
 ?>
 <!doctype html>
 <html lang="en">
