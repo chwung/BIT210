4fcbb49a194545d9f797993fec6a748adf054af5
chwung <weichung35@gmail.com>
2021-11-16 19:44:44 +0800

    Update setAppointment.php

--- setAppointment.php
+++ setAppointment.php
@@ -3,18 +3,32 @@
 
     include("dbcon.php");
 
-    $vac = $_SESSION['vac'];
+    if(isset($_SESSION['patientUsername']))
+    {
+        $username = $_SESSION['patientUsername'];
+        $password = $_SESSION['patientPassword'];
+        $query = "SELECT * FROM patients WHERE username = '$username' AND password = '$password'";
+
+        $result = $connection->query($query);
+        if($result -> num_rows > 0)
+        {
+            $user_data = $result -> fetch_assoc();
+        }
+    }
+
+    $vacid = $_SESSION['vacId'];
     $center = $_SESSION['center'];
     $username = $_SESSION['patientUsername'];
     $password = $_SESSION['patientPassword'];
     $_SESSION['appointmentDate'] = $_POST['appointmentDate'];
     $appointmentDate = $_SESSION['appointmentDate'];
+    $status = "pending";
+    $remarks = null;
 
-    echo $vac;
     echo $center;
     echo $username;
     echo $password;
     echo $appointmentDate;
 
-    
+    $query = "INSERT INTO appointments VALUES ('$vacid', '$status', '$appointmentDate', '$center', '$remarks' )"
 ?>
