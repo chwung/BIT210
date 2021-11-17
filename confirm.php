311da41d2b3edddf251e7838d08ac74328a83313
chwung <weichung35@gmail.com>
2021-11-17 11:56:19 +0800

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -2,18 +2,20 @@
 session_start();
 include("dbcon.php");
 
+$vacc = $_POST['vaccID'];
 $email = $_POST['email'];
 $batch_id = $_SESSION['batchID'];
 $vaccine = $_SESSION['appVaccine'];
 $expiry_date = $_SESSION['appExp'];
 
 if(isset($_POST['submit'])){
-    $query_data = "UPDATE appointments SET status='Confirmed' WHERE email = '$email'";
+    $query_data = "UPDATE appointments SET status='Confirmed' , vaccination_id='$vacc' WHERE email = '$email'";
     $connection->query($query_data);
 }else if(isset($_POST['reject'])){
     $query_data = "UPDATE appointments SET status= 'Rejected' WHERE email = '$email'";
     $connection->query($query_data);
 }
+
 
 header("location: viewVacBat.php?batch=$batch_id&vaccine=$vaccine&expiry=$expiry_date");
 
